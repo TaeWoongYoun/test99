4d05000a45d37201b519dbfed8c01b2f03f52c29
xodnd0507 <24457545yong@gmail.com>
2024-08-28 22:06:46 +0900

    test : Update

--- index.php
+++ index.php
@@ -7,23 +7,24 @@
     <title>Document</title>
 </head>
 <body>
-    <?php $id = isset($_GET['id']) ? $_GET['id'] : '';?>
     <?php
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-            $sql = "SELECT * FROM user WHERE id = '$id'";
-            $result = mysqli_query($conn, $sql);
-            if ($row = mysqli_fetch_array($result)) {
-                echo "
-                    <button><a href=''>{$row['name']}</a></button>
-                    <button><a href='index.php'>로그아웃</a></button>";
-            }
-        } else {
+    $id = isset($_GET['id']) ? $_GET['id'] : '';
+
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $sql = "SELECT * FROM user WHERE id = '$id'";
+        $result = mysqli_query($conn, $sql);
+        if ($row = mysqli_fetch_array($result)) {
             echo "
-            <button><a href='join.php'>회원가입</a></button>
-            <button><a href='login.php'>로그인</a></button>";
+            <button><a href=''>{$row['name']}</a></button>
+            <button><a href='index.php'>로그아웃</a></button>";
         }
-        ?>
+    } else {
+        echo "
+        <button><a href='join.php'>회원가입</a></button>
+        <button><a href='login.php'>로그인</a></button>";
+    }
+    ?>
         <button><a href="reservation.php?id=<?=$id?>">예약하기</a></button>
 </body>
 </html>
